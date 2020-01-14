54a71be3d27de26718b891e6d39a9daf6f7b0a16
Pavel Khankevich <v-pavel.k@turvo.com>
2020-01-14 15:47:04 +0300

    Initial commit

---
+++ src/main/java/com/letsdev/employeedevelopmentcards/EmployeeDevelopmentCardsApplication.java
@@ -0,0 +1,13 @@
+package com.letsdev.employeedevelopmentcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDevelopmentCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDevelopmentCardsApplication.class, args);
+	}
+
+}
